50b71504d20816656f648cdaabe756edcf8e7c12
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-03 18:48:11 +0000

    Refactor unit tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,23 +4,33 @@ describe Account do
   let(:stub_statement) { double(:statement, statement: []) }
   subject { Account.new(stub_statement) }
 
-  STATEMENT = ["date || credit || debit || balance", "02/12/2018 || 100.00 || || 100.00", "02/12/2018 || || 40.00 || 60.00"]
+  describe "#current_balance" do
+    it "starts with an opening balance of 0" do
+      expect(subject.current_balance).to eq '%.2f' % 0.00
+    end
+  end
 
-  it "starts with an opening balance of 0" do
-    expect(subject.current_balance).to eq ('%.2f' % 0.00)
+  describe "#deposit" do
+    it "money can be deposited into the account" do
+      expect(stub_statement).to receive(:deposit).with("100.00", "100.00")
+      subject.deposit(100.00)
+      expect(subject.current_balance).to eq '%.2f' % 100.00
+    end
   end
-  it "money can be deposited into the account" do
-    expect(stub_statement).to receive(:deposit).with("100.00", "100.00")
-    subject.deposit(100.00)
-    expect(subject.current_balance).to eq '%.2f' % 100.00
+
+  describe "#withdraw" do
+    it 'money can be withdrawn from the account' do
+      expect(stub_statement).to receive(:withdraw).with("-40.00", "40.00")
+      subject.withdraw(40.00)
+      expect(subject.current_balance).to eq '%.2f' % -40.00
+    end
   end
-  it 'money can be withdrawn from the account' do
-    expect(stub_statement).to receive(:withdraw).with("-40.00", "40.00")
-    subject.withdraw(40.00)
-    expect(subject.current_balance).to eq '%.2f' %  -40.00
-  end
-  it "returns the account statement with recent transactions" do
-    expect(stub_statement).to receive(:view_statement).and_return(STATEMENT)
-    expect(subject.view_statement).to eq STATEMENT
+
+  describe "#view_statement" do
+    it "returns the account statement with recent transactions" do
+      STATEMENT = ["date || credit || debit || balance", "02/12/2018 || 100.00 || || 100.00", "02/12/2018 || || 40.00 || 60.00"]
+      expect(stub_statement).to receive(:view_statement).and_return(STATEMENT)
+      expect(subject.view_statement).to eq STATEMENT
+    end
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,24 +1,33 @@
 require 'statement'
 
 describe Statement do
-
-  STATEMENT_1 = ["date || credit || debit || balance", "02/12/2018 || 100.00 || || 100.00"]
-  STATEMENT_2 = ["date || credit || debit || balance", "02/12/2018 || 100.00 || || 100.00", "02/12/2018 || || 40.00 || 60.00"]
-
-  statement = Statement.new
   let(:date) { "02/12/2018" }
+  subject { Statement.new }
 
   before do
     allow(Time).to receive_message_chain(:now, :strftime).and_return(date)
   end
 
-  it "updates the statment when a deposit is made" do
-    statement.deposit(100.00, 100.00)
-    expect(statement.view_statement).to eq STATEMENT_1
-  end
-  it "updates the statment when a withdrawal is made" do
-    statement.withdraw(60.00, 40.00)
-    expect(statement.view_statement).to eq STATEMENT_2
+  describe "#view_statement" do
+    it "returns the statment" do
+      STATEMENT_0 = ["date || credit || debit || balance"]
+      expect(subject.view_statement).to eq STATEMENT_0
+    end
   end
 
+  describe "#deposit" do
+    it "updates the statment when a deposit is made" do
+      STATEMENT_1 = ["date || credit || debit || balance", "02/12/2018 || 100.00 || || 100.00"]
+      subject.deposit(100.00, 100.00)
+      expect(subject.view_statement).to eq STATEMENT_1
+    end
+  end
+
+  describe "#withdraw" do
+    it "updates the statment when a withdrawal is made" do
+      STATEMENT_2 = ["date || credit || debit || balance", "02/12/2018 || || 40.00 || 60.00"]
+      subject.withdraw(60.00, 40.00)
+      expect(subject.view_statement).to eq STATEMENT_2
+    end
+  end
 end
