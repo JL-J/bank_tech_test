dc976bd88921438e345d86c8a3f5c00459fb2f63
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-03 17:25:27 +0000

    Creat and view statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,42 @@
 class Account
 
-  STARTING_BALANCE = 0
+  STARTING_BALANCE = 0.00
 
   def initialize
     @balance = STARTING_BALANCE
+    @statment = ["date || credit || debit || balance"]
   end
 
   def current_balance
-    @balance
+    '%.2f' % @balance
   end
 
   def deposit(amount)
     @balance += amount
+    update_statment("credit", '%.2f' % amount)
   end
 
   def withdraw(amount)
-    @balance -= amount    
+    @balance -= amount
+    update_statment("debit", '%.2f' % amount)
+  end
+
+  def view_statement
+    @statment
+  end
+
+  private
+
+  def update_statment(action, amount)
+    if action == "credit"
+      @statment.push("#{todays_date} || #{amount} || || #{current_balance}")
+    else
+      @statment.push("#{todays_date} || || #{amount} || #{current_balance}")
+    end
+  end
+
+  def todays_date
+    Time.now.strftime("%m/%d/%Y")
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,18 +1,27 @@
 require 'account'
 
 describe Account do
-  STARTING_BALANCE = 0
+  STARTING_BALANCE = '%.2f' % 0.00
+  STATMENT = ["date || credit || debit || balance", "02/12/2018 || 100.00 || || 100.00", "02/12/2018 || || 40.00 || 60.00"]
   account = Account.new
+
+  let(:date) { "02/12/2018" }
+  before do
+    allow(Time).to receive_message_chain(:now, :strftime).and_return(date)
+  end
 
   it "starts with an opening balance of 0" do
     expect(account.current_balance).to eq STARTING_BALANCE
   end
   it "money can be deposited into the account" do
-    account.deposit(100)
-    expect(account.current_balance).to eq 100
+    account.deposit(100.00)
+    expect(account.current_balance).to eq '%.2f' % 100.00
   end
   it 'money can be withdrawn from the account' do
-    account.withdraw(40)
-    expect(account.current_balance).to eq 60
+    account.withdraw(40.00)
+    expect(account.current_balance).to eq '%.2f' %  60.00
+  end
+  it "returns the account statment with recent transactions" do
+    expect(account.view_statement).to eq STATMENT
   end
 end
